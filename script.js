8861d5131db695aad5b5b8e5210196bf72c67c98
Bashsoft707 <alausabashir@gmail.com>
2020-09-07 03:20:14 +0100

    restructing webpage

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 
     var dropdownBtn = document.querySelector("#dropdown");
-var menuContent = document.querySelector(".my-dropdown")
+    var menuContent = document.querySelector(".my-dropdown")
     
-        // showParagraph.addEventListener("click", function() {
-        // menu.classList.toggle("show")
-        // })
-
-
     dropdownBtn.addEventListener("click", function () {
         if (menuContent.style.display==="") {
             menuContent.style.display="block";
